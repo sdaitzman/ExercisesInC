8e1834bbc8bb561ff25e75f52e70726fdaf66d8c
Sam Daitzman <sam@daitzman.com>
2020-04-14 21:32:09 -0400

    done with hw9!!

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -39,8 +39,11 @@ int main(int argc, char *argv[]) {
     int num_feeds = 5;
     char *search_phrase = argv[1];
     char var[255];
+    int status;
 
     for (int i=0; i<num_feeds; i++) {
+
+        // create forks
         pid = fork();
         if(pid < 0) {
             error("An error occurred in forking the process.");
@@ -48,10 +51,15 @@ int main(int argc, char *argv[]) {
             sprintf(var, "RSS_FEED=%s", feeds[i]);
             char *vars[] = {var, NULL};
             int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-            if (res == -1) {
-                error("Can't run script.");
-            }
+            if (res == -1) error("Can't run script.");
         }
+
+        // handle waits
+        pid = wait(&status);
+        if(pid < 0) error("wait() encountered some error.");
+        status = WEXITSTATUS(status);
+        if(status != 0) fprintf(stderr, "Process error: %d\n", status);
+
     }
     return 0;
 }
