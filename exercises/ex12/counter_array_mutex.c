11fe292138bd5d4ede0dc88a922a54b48eab55b8
Sam Daitzman <sam@daitzman.com>
2020-04-30 22:40:57 -0400

    code style changes

--- exercises/ex12/counter_array_mutex.c
+++ exercises/ex12/counter_array_mutex.c
@@ -12,18 +12,14 @@ License: GNU GPLv3
 
 #define NUM_CHILDREN 2
 
-void perror_exit(char *s)
-{
+void perror_exit(char *s) {
     perror(s);
     exit(-1);
 }
 
-void *check_malloc(int size)
-{
+void *check_malloc(int size) {
     void *p = malloc(size);
-    if (p == NULL) {
-        perror_exit("malloc failed");
-    }
+    if (p == NULL) perror_exit("malloc failed");
     return p;
 }
 
@@ -33,8 +29,7 @@ typedef struct {
     int *array;
 } Shared;
 
-Shared *make_shared(int end)
-{
+Shared *make_shared(int end) {
     int i;
     Shared *shared = check_malloc(sizeof(Shared));
 
@@ -42,59 +37,45 @@ Shared *make_shared(int end)
     shared->end = end;
 
     shared->array = check_malloc(shared->end * sizeof(int));
-    for (i=0; i<shared->end; i++) {
-        shared->array[i] = 0;
-    }
+
+    for (i=0; i<shared->end; i++) shared->array[i] = 0;
     return shared;
 }
 
-pthread_t make_thread(void *(*entry)(void *), Shared *shared)
-{
+pthread_t make_thread(void *(*entry)(void *), Shared *shared) {
     int ret;
     pthread_t thread;
 
     ret = pthread_create(&thread, NULL, entry, (void *) shared);
-    if (ret != 0) {
-        perror_exit("pthread_create failed");
-    }
+    if (ret != 0) perror_exit("pthread_create failed");
     return thread;
 }
 
-void join_thread(pthread_t thread)
-{
+void join_thread(pthread_t thread) {
     int ret = pthread_join(thread, NULL);
-    if (ret == -1) {
-        perror_exit("pthread_join failed");
-    }
+    if (ret == -1) perror_exit("pthread_join failed");
 }
 
-void child_code(Shared *shared)
-{
+void child_code(Shared *shared) {
     printf("Starting child at counter %d\n", shared->counter);
 
     while (1) {
-        if (shared->counter >= shared->end) {
-            return;
-        }
+        if (shared->counter >= shared->end) return;
         shared->array[shared->counter]++;
         shared->counter++;
 
-        if (shared->counter % 10000 == 0) {
-            printf("%d\n", shared->counter);
-        }
+        if (shared->counter % 10000 == 0) printf("%d\n", shared->counter);
     }
 }
 
-void *entry(void *arg)
-{
+void *entry(void *arg) {
     Shared *shared = (Shared *) arg;
     child_code(shared);
     printf("Child done.\n");
     pthread_exit(NULL);
 }
 
-void check_array(Shared *shared)
-{
+void check_array(Shared *shared) {
     int i, errors=0;
 
     printf("Checking...\n");
@@ -105,20 +86,15 @@ void check_array(Shared *shared)
     printf("%d errors.\n", errors);
 }
 
-int main()
-{
+int main() {
     int i;
     pthread_t child[NUM_CHILDREN];
 
     Shared *shared = make_shared(1000000);
 
-    for (i=0; i<NUM_CHILDREN; i++) {
-        child[i] = make_thread(entry, shared);
-    }
+    for (i=0; i<NUM_CHILDREN; i++) child[i] = make_thread(entry, shared);
 
-    for (i=0; i<NUM_CHILDREN; i++) {
-        join_thread(child[i]);
-    }
+    for (i=0; i<NUM_CHILDREN; i++) join_thread(child[i]);
 
     check_array(shared);
     return 0;
